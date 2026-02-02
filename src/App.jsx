1ca58245cf5dcc61f984e850f9c0ec90f26f36b4
Froggles <elmsterm@gmail.com>
2026-02-02 12:34:22 -0500

    create episode list component

--- src/App.jsx
+++ src/App.jsx
@@ -4,4 +4,27 @@ import { episodeList } from "./data";
 export default function App() {
   const [episodes] = useState(episodeList);
   const [selectedEpisode, setSelectedEpisode] = useState();
+
+  /** Shows a list of episodes */
+  function Episodes() {
+    return (
+      <section className="episodes">
+        <h2>Episodes</h2>
+        <ol className="episodes">
+          {episodes.map((episode) => (
+            <li
+              key={episode.id}
+              onClick={() => setSelectedEpisode(episode)}
+              className={
+                episode.id === selectedEpisode?.id ? "active" : "inactive"
+              }
+            >
+              {episode.title}
+            </li>
+          ))}
+        </ol>
+      </section>
+    );
+  }
+
 }
